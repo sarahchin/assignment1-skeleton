044d16923bc24cd787468d300c0fa2f7c9a324cf
sarahchin <42782570+sarahchin@users.noreply.github.com>
2018-09-15 23:37:13 -0400

    Update vm.c

--- vm.c
+++ vm.c
@@ -3,6 +3,7 @@
 // COP 3402, Fall 2018
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "vm.h"
 #include "data.h"
 
@@ -28,8 +29,7 @@ enum { CONT, HALT };
 void initVM(VirtualMachine* vm)
 {
 	int i;
-	vm = malloc(sizeof(VirtualMachine));
-
+	
 	vm->BP = 1;
 	vm->SP = 0;
 	vm->PC = 0;
@@ -44,7 +44,7 @@ void initVM(VirtualMachine* vm)
     if(vm == NULL)
     {
         printf("Error! Not enough memory. Could not create VM.\n");
-		exit();
+		return;
     }
 }
 
@@ -84,7 +84,17 @@ void dumpInstructions(FILE* out, Instruction* ins, int numOfIns)
 
 int getBasePointer(int *stack, int currentBP, int L)
 {
-    // TODO
+	int base;
+	
+	base = currentBP;
+	
+	while (L > 0)
+	{
+		base = stack[base + 1];
+		L--;
+	}
+	
+	return base;
 }
 
 // Function that dumps the whole stack into output file
@@ -121,35 +131,113 @@ void dumpStack(FILE* out, int* stack, int sp, int bp)
     }
 }
 
-int base(l, base)
-{
-	int b1;
-	
-	b1 = base;
-	
-	while (1 > 0)
-	{
-		b1 = stack[b1 + 1];
-		l--;
-	}
-	
-	return b1;
-}
-
 int executeInstruction(VirtualMachine* vm, Instruction ins, FILE* vmIn, FILE* vmOut)
 {
+	int level;
+	
     switch(ins.op)
     {
         case 1:
-			R[ins.r] = ins.m;
+			vm->RF[ins.r] = ins.m;
 			break;
 		case 2:
-			vm->SP = BP-1;
-			BP = vm->stack[vm->SP+3];
-			PC = vm->stack[vm->SP+4];
+			vm->SP = vm->BP-1;
+			vm->BP = vm->stack[vm->SP+3];
+			vm->PC = vm->stack[vm->SP+4];
 			break;
 		case 3:
-		
+			level = getBasePointer(vm->stack, vm->BP, ins.l);
+			level += ins.m;
+			vm->RF[ins.r] = vm->stack[level];
+			break;
+		case 4: 
+			level = getBasePointer(vm->stack, vm->BP, ins.l);
+			level += ins.m;
+			vm->stack[level] = vm->RF[ins.r];
+			break;
+		case 5: // CAL
+			vm->stack[vm->SP + 1] = 0;
+			vm->stack[vm->SP + 2] = getBasePointer(vm->stack, vm->BP, ins.l);
+			vm->stack[vm->SP + 3] = vm->BP;
+			vm->stack[vm->SP + 4] = vm->PC;
+			vm->BP = vm->SP + 1;
+			vm->PC = ins.m;
+			break;
+		case 6:
+			vm->SP = vm->SP + ins.m;
+			break;
+		case 7: // JMP
+			vm->PC = ins.m;
+			break;
+		case 8: // JPC
+			if (vm->RF[ins.r] == 0)
+				vm->PC = ins.m;
+			break;
+		case 9:
+			fprintf(vmOut, "%d", vm->RF[ins.r]);
+			break;
+		case 10:
+			fscanf(vmIn, "%d", &(vm->RF[ins.r]));
+			break;
+		case 11: 
+			return HALT;
+		case 12:
+			vm->RF[ins.r] = vm->RF[ins.l];
+			break;
+		case 13:
+			vm->RF[ins.r] = vm->RF[ins.l] + vm->RF[ins.m];
+			break;
+		case 14:
+			vm->RF[ins.r] = vm->RF[ins.l] - vm->RF[ins.m];
+			break;
+		case 15:
+			vm->RF[ins.r] = vm->RF[ins.l] * vm->RF[ins.m];
+			break;
+		case 16:
+			vm->RF[ins.r] = vm->RF[ins.l] / vm->RF[ins.m];
+			break;
+		case 17:
+			vm->RF[ins.r] = vm->RF[ins.r]%2;
+			break;
+		case 18:
+			vm->RF[ins.r] = vm->RF[ins.l] % vm->RF[ins.m];
+			break;
+		case 19:
+			if (vm->RF[ins.l] == vm->RF[ins.m])
+				vm->RF[ins.r] = 1;
+			else
+				vm->RF[ins.r] = 0;
+			break;
+		case 20:
+			if (vm->RF[ins.l] == vm->RF[ins.m])
+				vm->RF[ins.r] = 0;
+			else
+				vm->RF[ins.r] = 1;
+			break;
+		case 21:
+			if (vm->RF[ins.l] < vm->RF[ins.m])
+				vm->RF[ins.r] = 1;
+			else
+				vm->RF[ins.r] = 0;
+			break;
+		case 22:
+			if (vm->RF[ins.l] <= vm->RF[ins.m])
+				vm->RF[ins.r] = 1;
+			else
+				vm->RF[ins.r] = 0;
+			break;
+		case 23:
+			if (vm->RF[ins.l] > vm->RF[ins.m])
+				vm->RF[ins.r] = 1;
+			else
+				vm->RF[ins.r] = 0;
+			break;
+		case 24:
+			if (vm->RF[ins.l] >= vm->RF[ins.m])
+				vm->RF[ins.r] = 1;
+			else
+				vm->RF[ins.r] = 0;
+			break;
         default:
             fprintf(stderr, "Illegal instruction?");
             return HALT;
@@ -160,8 +248,12 @@ int executeInstruction(VirtualMachine* vm, Instruction ins, FILE* vmIn, FILE* vm
 
 void simulateVM(FILE* inp, FILE* outp, FILE* vm_inp, FILE* vm_outp)
 {
-	int numOfIns = 0;
+	int i = 0, flag = CONT, numOfIns = 0;
+	
+	VirtualMachine* vm = NULL;
 	Instruction *ins = NULL;
+	
+	// Create Instruction array. (The code store).
 	ins = malloc(sizeof(Instruction) * MAX_CODE_LENGTH);
 	
 	// Read instructions from file
@@ -172,27 +264,51 @@ void simulateVM(FILE* inp, FILE* outp, FILE* vm_inp, FILE* vm_outp)
 
     // Before starting the code execution on the virtual machine,
     // .. write the header for the simulation part (***Execution***)
-    fprintf(outp, "\n***Execution***\n");
+	fprintf(outp, "\n***Execution***\n");
     fprintf
 	(
-        outp,
-        "%3s %3s %3s %3s %3s %3s %3s %3s %3s \n",         // formatting
-        "#", "OP", "R", "L", "M", "PC", "BP", "SP", "STK" // titles
-    );
+		outp,
+		"%3s %3s %3s %3s %3s %3s %3s %3s %3s \n",         // formatting
+		"#", "OP", "R", "L", "M", "PC", "BP", "SP", "STK" // titles
+	);
 
     // Create a virtual machine
-    VirtualMachine* vm = NULL;
+	vm = malloc(sizeof(VirtualMachine));
 
     // Initialize the virtual machine
     initVM(vm);
 
-    // Fetch&Execute the instructions on the virtual machine until halting
+	// Fetch & Execute the instructions on the virtual machine until halting
     while(flag == CONT)
     {
-		flag = executeInstruction(vm, ins, vmIn, vmOut);
-        fprintf(outp, "\n");
+		// Fetch
+		i = vm->PC;
+		
+		// Advance PC - before execution!
+		vm->PC += 1;
+		
+		// Execute the instruction
+		flag = executeInstruction(vm, ins[i], vm_inp, vm_outp);
+		
+		// Print current state
+		fprintf
+		(
+			outp,
+            "%3d %3s %3d %3d %3d %3d %3d %3d ", i,
+			opcodes[ins[i].op], ins[i].r, ins[i].l, ins[i].m,
+             vm->PC, vm->BP, vm->SP
+        );
+		
+		// Print stack info
+		dumpStack(outp, vm->stack, vm->SP, vm->BP);
+
+		fprintf(outp, "\n");
     }
 
     fprintf(outp, "HLT\n");
+	
+	free(ins);
+	free(vm);
+	
     return;
 }
